273c1677c04d5e9bf8b3ef183e389875dff4f25c
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2021-10-06 17:17:41 +0200

    try to fetch data and render posts

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader);
+
+$client = new Client([
+    'base_uri' => 'https://jsonplaceholder.typicode.com',
+]);
+
+$response = $client->request('GET', 'posts');
+$body = $response->getBody();
+$posts = json_decode($body->getContents());
+
+echo $twig->render('index.html.twig', [
+  'posts' => $posts
+]);
